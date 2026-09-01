6243184ddf4478a0da6c29e572c17c4ddddd41cc
Noob0639 <69603715+Noob0639@users.noreply.github.com>
2026-09-01 22:30:23 +0200

    Call updateClock function immediately on load

--- projects/clock/clock.js
+++ projects/clock/clock.js
@@ -13,4 +13,6 @@ function updateClock() {
     time.textContent = `${String(hour).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 }
 
+updateClock();
+
 setInterval(updateClock, 1000);
